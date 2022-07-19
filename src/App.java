304c3d6f67d47603e5d44dd2116b32e92280b3cc
petersonwpm <>
2022-07-18 21:14:18 -0300

    menu de opções e listagem de series

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -5,12 +6,68 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class App {
-    private static HttpResponse<String> response;
-
     public static void main(String[] args) throws Exception {
+        
+        System.out.println("### MENU ####");
+        System.err.println("0 - Sair");
+        System.out.println("1 - Listar os melhores filmes");
+        System.out.println("2 - Listar as melhores series");
 
+        System.err.print("Qual a sua escolha?: ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int opt = scanner.nextInt();
+
+        scanner.close();
+
+        switch (opt) {
+            case 0:
+                // end
+                break;
+            
+            case 1:
+                getMovies();
+            
+            case 2:
+                getSeries();
+        
+            default:
+                break;
+        }
+        
+        
+
+    }
+
+    private static void getSeries() throws IOException, InterruptedException {
+        URI url = URI.create("https://api.mocki.io/v2/549a5d8b/Top250TVs");
+
+        HttpClient http = HttpClient.newHttpClient();
+        HttpResponse<String> response = http.send(HttpRequest
+                .newBuilder()
+                .uri(url)
+                .build(),
+                BodyHandlers.ofString());
+
+        String body = response.body();
+
+
+        JsonParser parser = new JsonParser();
+
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("year"));
+            System.out.println(filme.get("imDbRating"));
+        }
+    }
+
+    private static void getMovies() throws IOException, InterruptedException {
         URI url = URI.create("https://api.mocki.io/v2/549a5d8b");
 
         HttpClient http = HttpClient.newHttpClient();
