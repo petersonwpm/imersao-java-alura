b7f40b75cbd3fe528828e28207c9d0e6a86ee09c
petersonwpm <>
2022-07-19 18:48:24 -0300

    corrige erro no switch

--- src/App.java
+++ src/App.java
@@ -7,11 +7,7 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         int opt;
         do {
-            System.out.println("\n\n### \uD83D\uDCFA MENU \uD83D\uDCFA ####");
-            System.err.println("0 - Sair");
-            System.out.println("1 - Listar os melhores filmes");
-            System.out.println("2 - Listar as melhores series");
-            System.err.print("Qual a sua escolha?: ");
+            View.menu();
 
             opt = scanner.nextInt();
 
@@ -24,18 +20,20 @@ public class App {
                 case 1:
                     List<Map<String, String>> movies = Api.getMovies();
                     for (Map<String, String> movie : movies) {
+                        System.out.println(movie);
                         View.show(movie);
                     }
+                    break;
                 case 2:
                     List<Map<String, String>> series = Api.getSeries();
                     for (Map<String, String> serie : series) {
                         View.show(serie);
                     }
+                    break;
 
                 default:
                     break;
             }
         } while (opt != 0);
     }
-
 }
