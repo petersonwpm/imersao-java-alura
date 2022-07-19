3ea33b314443ee1f3697dedb44c3d3fab08cfa2b
petersonwpm <>
2022-07-19 19:08:08 -0300

    realiza o parser do json

--- src/Api.java
+++ src/Api.java
@@ -10,11 +10,16 @@ public class Api {
 
     public static List<Map<String, String>> getSeries() throws IOException, InterruptedException {
         Http http = new Http(URI.create("https://api.mocki.io/v2/549a5d8b/Top250TVs"));
-        return http.get();
+        JsonParser parser = new JsonParser();
+
+        return parser.parse(http.get().body());
     }
 
     public static List<Map<String, String>> getMovies() throws IOException, InterruptedException {
         Http http = new Http(URI.create("https://api.mocki.io/v2/549a5d8b/Top250Movies"));
-        return http.get();
+        JsonParser parser = new JsonParser();
+
+        return parser.parse(http.get().body());
+
     }
 }
