6c0a3ff800455755caf3bd269088ddccba394cdf
petersonwpm <>
2022-07-18 22:59:07 -0300

    exibe menu enquanto usuário ñ optar por sair

--- src/App.java
+++ src/App.java
@@ -4,37 +4,38 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
-        System.out.println("### MENU ####");
-        System.err.println("0 - Sair");
-        System.out.println("1 - Listar os melhores filmes");
-        System.out.println("2 - Listar as melhores series");
-        System.err.print("Qual a sua escolha?: ");
-
         Scanner scanner = new Scanner(System.in);
+        int opt;
+        do {
+            System.out.println("\n\n### \uD83D\uDCFA MENU \uD83D\uDCFA ####");
+            System.err.println("0 - Sair");
+            System.out.println("1 - Listar os melhores filmes");
+            System.out.println("2 - Listar as melhores series");
+            System.err.print("Qual a sua escolha?: ");
 
-        int opt = scanner.nextInt();
+            opt = scanner.nextInt();
 
-        scanner.close();
+            switch (opt) {
+                case 0:
+                    // end
+                    scanner.close();
+                    break;
 
-        switch (opt) {
-            case 0:
-                // end
-                break;
+                case 1:
+                    List<Map<String, String>> movies = Api.getMovies();
+                    for (Map<String, String> movie : movies) {
+                        View.show(movie);
+                    }
+                case 2:
+                    List<Map<String, String>> series = Api.getSeries();
+                    for (Map<String, String> serie : series) {
+                        View.show(serie);
+                    }
 
-            case 1:
-                List<Map<String, String>> movies = Api.getMovies();
-                for (Map<String, String> movie : movies) {
-                    View.show(movie);
-                }
-            case 2:
-                List<Map<String, String>> series = Api.getSeries();
-                for (Map<String, String> serie : series) {
-                    View.show(serie);
-                }
-
-            default:
-                break;
-        }
+                default:
+                    break;
+            }
+        } while (opt != 0);
     }
+
 }
--- src/View.java
+++ src/View.java
@@ -2,11 +2,11 @@ import java.util.Map;
 
 public class View {
     public static void show(Map<String, String> item) {
-        System.out.println("\n");
+        System.out.println("\n \uD83D\uDCFC");
         System.out.println("Titulo: " + item.get("title"));
         System.out.println("Ano: " + item.get("year"));
         System.out.println(item.get("imDbRating"));
-        // Integer rating = Math.round(Float.parseFloat(item.get("imDbRating")));
+        
 
         System.out.print("\u2B50".repeat((int) Float.parseFloat(item.get("imDbRating"))));    
 
