76a3c3634bcbcc8a1f2b92ddac703562e63a42a0
petersonwpm <>
2022-07-19 19:03:59 -0300

    remove parser da classe http

--- src/Http.java
+++ src/Http.java
@@ -4,8 +4,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Http
@@ -18,21 +16,13 @@ public class Http {
         this.uri = uri;
     }
 
-    public List<Map<String, String>> get() throws IOException, InterruptedException {
-        HttpClient http = HttpClient.newHttpClient();
-        HttpResponse<String> response;
-
-        response = http.send(HttpRequest
+    public HttpResponse<String> get() throws IOException, InterruptedException {
+        return HttpClient.newHttpClient()
+            .send(HttpRequest
                 .newBuilder()
                 .uri(this.uri)
                 .build(),
                 BodyHandlers.ofString());
-
-        String body = response.body();
-
-        JsonParser parser = new JsonParser();
-
-        return parser.parse(body);
     }
 
 }
