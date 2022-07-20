3152dabc8c337c19cce6ea89725f420ebfc9aef2
petersonwpm <>
2022-07-19 21:36:18 -0300

    cria novo graphics e escreve no buffer antes de escrever em disco

--- src/App.java
+++ src/App.java
@@ -2,11 +2,14 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
+
+import javax.imageio.ImageIO;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -28,30 +31,35 @@ public class App {
                     for (Map<String, String> movie : movies) {
 
                         URL url = new URL(movie.get("image"));
+                        /** @todo: identificar o mime type */
                         InputStream input = url.openStream();
+                        System.out.println( url.getUserInfo());
 
-                        String movieName = movie.get("title").toLowerCase().replaceAll("\s","-") + ".jpg";
+                        String movieName = movie.get("title").toLowerCase().replaceAll("[.',:]","").replaceAll("[\s]","-") + ".png";
 
                         File f =  new File(movieName);
                         File currentDir = new File(System.getProperty("user.dir"));
 
+                        BufferedImage image = ImageIO.read(input);
+
+                        Graphics2D graphics = image.createGraphics();
+
+                        graphics.drawString(movieName, 100, 100);
+
                         File out = new File(currentDir.getParent(), "output"); 
-                        
-                        if (!out.exists() && !out.canWrite()) throw new IOException("Não é possivel criar o diretório" + out.getPath());
+
+                        System.out.println(out.getPath());
+                            
+                        if (!out.exists() && !out.canWrite()) throw new IOException("Não é possivel criar o diretório " + out.getPath());
 
                         out.mkdir();
 
+                        File output = new File(out.getPath() +"/"+ f.getName());
 
-                        System.out.println(out.getPath() +"/"+ f.getName());
-                        // if (!f.canWrite()) throw new IOException();
-
-                        FileOutputStream output = new FileOutputStream(out.getPath() +"/"+ f.getName());
-
-                        output.write(input.readAllBytes());
-
-                        output.flush();
+                        // output.write(input.readAllBytes());
+                        ImageIO.write( image, "png", output); // output.flush();
                         input.close();
-                        output.close();
+                        // output.close();
 
                         View.show(movie);
                     }
